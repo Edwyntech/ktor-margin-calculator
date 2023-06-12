d7ba9ac07b51ccb922347b8f995dd6e389105ea3
caroline chuong <pandoraangora@gmail.com>
2023-06-12 14:33:20 +0200

    Init project with dependencies

---
+++ src/main/kotlin/com/margin/Application.kt
@@ -0,0 +1,14 @@
+package com.margin
+
+import io.ktor.server.application.*
+import com.margin.plugins.*
+import io.ktor.server.application.install
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
